0f167a445dfc1062103e524ceb8cc3544d1ed20d
Gelbpunkt <jens@troet.org>
2018-10-27 15:59:31 +0000

    working version

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var routes = require("./routes/routes.js");
+var app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    if (app.gameDone) {
+        return res.status(500).json({"status": "error", "text": "game ended"});
+    }
+    next();
+})
+
+routes(app);
+
+app.bots = [{name: "Dyno", hp: 100}, {name: "Naoko", hp: 100}, {name: "IdleRPG", hp: 100}, {name: "Rythm", hp: 100}]
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+shuffle(app.bots);
+
+app.map = Array(40).fill(null).map(e => Array(40).fill(null));
+app.map[0][0] = app.bots[0];
+app.map[0][39] = app.bots[1];
+app.map[39][0] = app.bots[2];
+app.map[39][39] = app.bots[3];
+
+app.gameDone = false;
+
+var server = app.listen(2312, function () {
+    console.log("app running on port.", server.address().port);
+});
---
+++ routes/routes.js
@@ -0,0 +1,128 @@
+const { table } = require("table")
+
+function remove(array, element) {
+    const index = array.indexOf(element);
+
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+}
+
+var appRouter = function (app) {
+    app.get("/", function(req, res) {
+        res.status(404).json({"status": "error", "text": "call an endpoint please"});
+    });
+
+    app.get("/map", function (req, res) {
+        res.status(200).json(app.map);
+    });
+
+    app.get("/turn", function (req, res) {
+        res.status(200).json(app.bots[0]);
+    });
+
+    app.get("/prettymap", function(req, res) {
+        res.setHeader('Content-Type', 'text/plain');
+        var text = "";
+        app.bots.forEach(function (bot) {
+            text = text + `${bot.name}: ${bot.hp} HP\n`;
+        });
+        var out = [];
+        app.map.forEach(function (row) {
+            var new_row = [];
+            row.forEach(function (column) {
+                if (column == null) {
+                    new_row.push(" ");
+                }
+                else {
+                    new_row.push(column.name);
+                }
+            });
+            out.push(new_row);
+        });
+        text = text + table(out);
+        res.status(200).send(text);
+    });
+
+    app.post("/move", function (req, res) {
+        var direction = req.body.direction;
+        var action = req.body.action;
+        var bot = req.body.bot;
+        if (bot != app.bots[0].name) {
+            return res.status(403).json({"status": "error", "text": "not your move yet"});
+        }
+        if (!["North", "South", "West", "East"].includes(direction)) {
+            return res.status(500).json({"status": "error", "text": "not a valid direction"});
+        }
+        if (!["Attack", "Move"].includes(action)) {
+            return res.status(500).json({"status": "error", "text": "not a valid action"});
+        }
+        loop:
+            for(var i=0; i<40; i++) {
+        loop2:
+                for(var j=0; j<40; j++) {
+                    if (app.map[i][j]) {
+                        if (app.map[i][j].name == bot) {
+                            var y = i;
+                            var x = j;
+                            break loop;
+                        }
+                    }
+                }
+            }
+        old_x = x;
+        old_y = y;
+        if (direction == "North" && y != 0) {
+            y = y - 1;
+        }
+        if (direction == "South" && y != 39) {
+            y = y + 1;
+        }
+        if (direction == "West" && x != 0) {
+            x = x - 1;
+        }
+        if (direction == "East" && x != 39) {
+            x = x + 1;
+        }
+        if (action == "Move") {
+            if (app.map[y][x]) {
+                res.status(500).json({"status": "error", "text": "can't move on enemy"});
+            }
+            else {
+                app.map[old_y][old_x] = null;
+                app.map[y][x] = app.bots[0];
+                res.status(200).json({"status": "done", "text": `moved to ${x}, ${y}`});
+            }
+        }
+        else {
+            if (app.map[y][x]) {
+                var dmg = Math.floor(Math.random() * 20) + 1;
+                app.map[y][x].hp = app.map[y][x].hp - dmg;
+                if (app.map[y][x].hp > 0) {
+                    res.status(200).json({"status": "done", "text": `hit ${app.map[y][x].name} for ${dmg} damage`});
+                }
+                else {
+                    remove(app.bots, app.map[y][x]);
+                    app.map[y][x] = null;
+                }
+            }
+            else {
+                res.status(500).json({"status": "error", "text": "nothing to attack"});
+            }
+        }
+        if (app.bots.length == 4) {
+            app.bots = [app.bots[1], app.bots[2], app.bots[3], app.bots[0]];
+        }
+        if (app.bots.length == 3) {
+            app.bots = [app.bots[1], app.bots[2], app.bots[0]];
+        }
+        if (app.bots.length == 2) {
+            app.bots = [app.bots[1], app.bots[0]];
+        }
+        if (app.bots.length == 1) {
+            app.gameDone = true;
+        }
+    });
+}
+
+module.exports = appRouter;
